547e531c8a4792950d8335937f666840d9fe9924
NaBest85 <nicholasbest85@gmail.com>
2023-07-05 11:25:55 -0400

    Commit My Changes

--- main.py
+++ main.py
@@ -13,51 +13,58 @@ with open(r'C:\Users\nicho\OneDrive\Desktop\budget_data.csv') as csvfile:
     First_Row = next(datareader)
     Total_Pl += int(First_Row[1])
     Value = int(First_Row[1])
-    #print()
-    for row in datareader:
+    Total_Months += 1
+
+
+    for row in datareader:     
+        
+        
+        Total_Months += 1
         Dates.append(row[0])
-        Value = int(row[1])
+
+     
         Change = int(row[1])-Value
         Profits.append(Change)
-        Total_Months += 1
-
+        Value = int(row[1])
         Total_Pl = Total_Pl + int(row[1])
-
-        Avg_Change = sum(Profits)/len(Profits)
-        Greatest_Inc = max(Profits)
-        Greatest_Dec = min(Profits)
-        Decrease_Date = Dates[Greatest_Dec]
-        Increase_Date = Dates[Greatest_Inc]
-
-        output = open("output.txt", "w")
-
-        line1 = "Financial Analysis"
-        line2 = "----------------------------------"
-        #line3 = str("Total Months: [str(Total_Months)]")
-        #line4 = str("Total: $[str(Total_Pl)]")
-        #line5 = str("Average Change: $[str(round*(Avg_Change,2))]")
-        #line6 = str("Greatest Increase in Profits: [Increase_Date] ($[str(Greatest_Inc)])")
-        #line7 = str("Greatest Increase in Profits: [Decrease_Date] ($[str(Greatest_Dec)])")
-        #output.write(format(line1,line2,line3,line4,line5,line6,line7))
-        #output.write("[]\n[]\n[]\n[]\n[]\n[]\n[]\n".format(line1,line2,line3,line4,line5,line6,line7))
-
-        line1 = "Financial Analysis"
-        line2 = "----------------------------------"
-        line3 = "Total Months: " + " = len(Dates)"
-        line4 = "Total: $" + str(Total_Pl)
-        line5 = "Average Change: $" + str(round(Avg_Change,2))
-        line6 = "Greatest Increase in Profits: " + Increase_Date + " ($" + str(Greatest_Inc) + ")"
-        line7 = "Greatest Increase in Profits: " + Decrease_Date + " ($" + str(Greatest_Dec) + ")"
-        output.write("{}\n{}\n{}\n{}\n{}\n{}\n{}\n".format(line1,line2,line3,line4,line5,line6,line7))
+        
+        
+       
+        Avg_Change = sum(Profits)/len(Profits)   
+       
+        
+        
+        
+       
+        Greatest_Inc = max(Profits)       
+        Greatest_Inc_index = Profits.index(Greatest_Inc)        
+        Increase_Date_index = Dates[Greatest_Inc_index]
 
 
-        print("Financial Analysis")
-        print("---------------------")
-        print(f"Total Months: {str(Total_Months)}")
-        print(f"Total: ${str(Total_Pl)}")
-        print(f"Average Change: ${str(round(Avg_Change,2))}")
-        print(f"Greatest Increase in Profits: {Increase_Date} (${str(Greatest_Inc)})")
-        print(f"Greatest Decrease in Profits: {Decrease_date} (${str(Greatest_Dec)})")
+        Greatest_Dec = min(Profits)       
+        Greatest_Dec_index = Profits.index(Greatest_Dec)
+        Decrease_Date_index = Dates[Greatest_Dec_index]
+
+
+        output = open("output.txt", "w")        
+
+    line1 = "Financial Analysis"
+    line2 = "----------------------------------"
+    line3 = "Total Months: " + " = len(Dates)"
+    line4 = "Total: $" + str(Total_Pl)
+    line5 = "Average Change: $" + str(round(Avg_Change,2))
+    line6 = "Greatest Increase in Profits: " + Increase_Date_index + " ($" + str(Greatest_Inc) + ")"
+    line7 = "Greatest Increase in Profits: " + Decrease_Date_index + " ($" + str(Greatest_Dec) + ")"
+    output.write("{}\n{}\n{}\n{}\n{}\n{}\n{}\n".format(line1,line2,line3,line4,line5,line6,line7))
+
+
+    print("Financial Analysis")
+    print("---------------------")
+    print(f"Total Months: {str(Total_Months)}")
+    print(f"Total: ${str(Total_Pl)}")
+    print(f"Average Change: ${str(round(Avg_Change,2))}")
+    print(f"Greatest Increase in Profits: {Increase_Date_index} (${str(Greatest_Inc)})")
+    print(f"Greatest Decrease in Profits: {Decrease_Date_index} (${str(Greatest_Dec)})")
 
 
 
